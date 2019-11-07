9ab78565535eebf8017e477d6f0cd9605f3d3682
Belavusau, Ilya <ilya.belavusau@kronos.com>
2019-11-07 15:10:40 +0300

    flag build folder to root

--- src/store/actions/common.action.js
+++
@@ -1,3 +0,0 @@
-export const switchActiveFilter = (filter) => dispatch => {
-        dispatch({type: 'SWITCH_ACTIVE_FILTER', payload: filter });
-};
--- src/store/actions/gerens.action.js
+++
@@ -1,6 +0,0 @@
-import HttpClient from '../../services/movies.httpClient';
-
-export const fetchGenresList = () => dispatch => {
-    return HttpClient.getGenresList()
-        .then(res => dispatch({type: 'ADD_GENRES_LIST', payload: res.genres}));
-};
