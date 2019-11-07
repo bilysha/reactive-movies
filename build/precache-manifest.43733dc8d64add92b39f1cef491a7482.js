5022bf31d91528edccda264d05ca9d8d9a59354c
Belavusau, Ilya <ilya.belavusau@kronos.com>
2019-11-07 15:02:21 +0300

    create gh-page branch with app build

---
+++ build/precache-manifest.43733dc8d64add92b39f1cef491a7482.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "5d0e06701ab2b2c7de08",
+    "url": "/static/css/main.dbead933.chunk.css"
+  },
+  {
+    "revision": "b15df1e473528f8032a7",
+    "url": "/static/js/1.b15df1e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6d654fbad134c0523a6ac84ed0566cad",
+    "url": "/static/media/no_backdrop_image_avaliable.6d654fba.jpg"
+  },
+  {
+    "revision": "5d0e06701ab2b2c7de08",
+    "url": "/static/js/main.5d0e0670.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "8164760d9c9fc3205fc8bf41305779ab",
+    "url": "/index.html"
+  }
+];
